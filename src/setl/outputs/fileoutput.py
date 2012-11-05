f5af715d332651c485becdc906e5892e6034efa7
Just van den Broecke <just@justobjects.nl>
2012-11-05 15:33:21 +0100

    added multifile output

--- src/setl/outputs/fileoutput.py
+++ src/setl/outputs/fileoutput.py
@@ -23,10 +23,28 @@ class FileOutput(Output):
              return packet
 
         file_path = self.cfg.get('file_path')
+        return self.write_file(packet, file_path)
+
+    def write_file(self, packet, file_path):
         log.info('writing to file %s' % file_path)
         out_file = open(file_path, 'w')
         out_file.writelines(self.to_string(packet.data))
         out_file.close()
         log.info("written to %s" % file_path)
         return packet
+
+# Pretty print XML to multiple files
+class MultiFileOutput(FileOutput):
+    def __init__(self, configdict, section):
+        Output.__init__(self, configdict, section, consumes=FORMAT.etree_doc)
+        self.file_num = 1
+
+    def write(self, packet):
+        if packet.data is None:
+             return packet
+
+        file_path = self.cfg.get('file_path')
+        file_path = file_path % self.file_num
+        self.file_num += 1
+        return self.write_file(packet, file_path)
 
