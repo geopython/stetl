9d5dfda30622cde73331c28c02ed9faa947e12df
Just van den Broecke <just@justobjects.nl>
2014-06-07 13:55:33 +0200

    improved httpoutput.py with auth etc

--- stetl/outputs/httpoutput.py
+++ stetl/outputs/httpoutput.py
@@ -9,6 +9,7 @@ from stetl.output import Output
 from stetl.util import Util
 from stetl.packet import FORMAT
 import httplib
+import base64
 
 log = Util.get_log('httpoutput')
 
@@ -18,15 +19,16 @@ class HttpOutput(Output):
 
     consumes=FORMAT.etree_doc
     """
-
-    headers = {"Content−type": "Content-type: text/xml", "Accept": "text/xml"}
-
     def __init__(self, configdict, section, consumes=FORMAT.any):
         Output.__init__(self, configdict, section, consumes)
         self.host = self.cfg.get('host')
         self.port = self.cfg.get('port', '80')
         self.path = self.cfg.get('path')
         self.method = self.cfg.get('method', 'POST')
+        self.user = self.cfg.get('user', None)
+        self.password = self.cfg.get('password', None)
+        self.content_type = self.cfg.get('content_type', 'text/xml')
+        # self.accept_type = self.cfg.get('accept_type', self.content_type)
 
         # If we receive a list(), should we create a HTTP req for each member?
         self.list_fanout = self.cfg.get_bool('list_fanout', True)
@@ -34,18 +36,41 @@ class HttpOutput(Output):
     def create_payload(self, packet):
         return packet.data
 
-    def get_headers(self, packet):
-        return HttpOutput.headers
+    def post(self, packet, payload):
+        webservice = httplib.HTTP(self.host)
+        # write your headers
+        webservice.putrequest(self.method, self.path)
+        webservice.putheader("Host", self.host)
+        webservice.putheader("User-Agent", "Stetl Python http")
+        webservice.putheader("Content-Type", self.content_type)
+        # webservice.putheader("Accept", self.accept_type)
+        webservice.putheader("Content-Length", "%d" % len(payload))
 
-    def post(self, packet, payload, headers):
+        # basic auth: http://mozgovipc.blogspot.nl/2012/06/python-http-basic-authentication-with.html
+        # base64 encode the username and password
+        # write the Authorization header like: 'Basic base64encode(username + ':' + password)
+        if self.user is not None:
+            auth = base64.encodestring('%s:%s' % (self.user, self.password)).replace('\n', '')
+            webservice.putheader("Authorization", "Basic %s" % auth)
 
-        conn = httplib.HTTPConnection(self.host, self.port)
-        conn.request(self.method, self.path, payload, headers)
+        webservice.endheaders()
+        webservice.send(payload)
 
-        response = conn.getresponse()
-        log.info('status=%s msg=%s' % (response.status, response.msg))
-        log.info('response=%s' % response.read(1024))
-        conn.close()
+        # get the response
+        statuscode, statusmessage, header = webservice.getreply()
+        log.info("Response status: code=%d msg=%s" % (statuscode, statusmessage))
+        if statuscode != 200:
+            log.error("Headers: %s" % str(header))
+            res = webservice.getfile().read()
+            log.info('Content: %s' % res)
+
+        # conn = httplib.HTTPConnection(self.host, self.port)
+        # conn.request(self.method, self.path, payload, headers)
+
+        # response = conn.getresponse()
+        # log.info('status=%s msg=%s' % (response.status, response.msg))
+        # log.info('response=%s' % response.read(1024))
+        # conn.close()
         return packet
 
     def write(self, packet):
@@ -57,11 +82,11 @@ class HttpOutput(Output):
                 original_data = packet.data
                 for data_elm in original_data:
                     packet.data = data_elm
-                    self.post(packet, self.create_payload(packet), self.get_headers(packet))
+                    self.post(packet, self.create_payload(packet))
                     packet.data = original_data
 
         else:
             # Regular, single data element or list_fanout is False
-            self.post(packet, self.create_payload(packet), self.get_headers(packet))
+            self.post(packet, self.create_payload(packet))
 
         return packet
