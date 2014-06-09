a03317e09ea3ff88b51d1090bcdacdb2eff7ea14
Just van den Broecke <just@justobjects.nl>
2014-06-09 15:21:16 +0200

    improved dbinput.py : split into functions

--- stetl/inputs/dbinput.py
+++ stetl/inputs/dbinput.py
@@ -53,25 +53,32 @@ class PostgresDbInput(Input):
     def exit(self):
         # Disconnect from DB when done
         log.info('Exit: disconnect from DB')
+
         self.db.disconnect()
 
-    def read(self, packet):
-        recs_len = self.db.execute(self.query)
-        recs = self.db.cursor.fetchall()
-        log.info('read recs: %d' % recs_len)
+    def do_query(self, query_str):
+
+        self.db.execute(query_str)
+
+        db_records = self.db.cursor.fetchall()
+        log.info('read recs: %d' % len(db_records))
 
         # record is Python list of Python dict (multiple records)
-        packet.data = list()
+        records = list()
 
         # Convert list of lists to list of dict using column_names
-        for record in recs:
-            packet.data.append(dict(zip(self.column_names, record)))
+        for db_record in db_records:
+            records.append(dict(zip(self.column_names, db_record)))
+
+        return records
+
+    def read(self, packet):
+        packet.data = self.do_query(self.query)
 
         # No more records to process?
-        if recs_len == 0 or self.read_once is True:
+        if len(packet.data) == 0 or self.read_once is True:
             packet.set_end_of_stream()
             log.info('Nothing to do. All file_records done')
-
             return packet
 
         return packet
