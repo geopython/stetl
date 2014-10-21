8a2ea9c2453892da1b6175c2d0b4131389d538ac
Just van den Broecke <just@justobjects.nl>
2014-10-21 12:43:20 +0200

    issue #20 - add SQLite INput Component plus example

---
+++ examples/basics/13_dbinput/input/cities.sql
@@ -0,0 +1,9 @@
+-- Used to create the SQLite file cities.sdb
+
+-- to create the SQLite file cities.sdb invoke command
+--   sqlite3 cities.sdb < cities.sdb
+
+create table cities(name varchar(24), lat float, lon lat float);
+insert into cities values('Amsterdam',52.4,4.9);
+insert into cities values('Bonn',50.7,7.1);
+insert into cities values('Rome',41.9,12.5);
--- stetl/inputs/dbinput.py
+++ stetl/inputs/dbinput.py
@@ -25,13 +25,9 @@ class DbInput(Input):
         return packet
 
 
-class PostgresDbInput(Input):
+class SqlDbInput(DbInput):
     """
-    Input by querying records from a Postgres database.
-    Input is a query, like SELECT * from mytable.
-    Output is zero or more records.
-
-    produces=FORMAT.record
+    Input using a query from any SQL-based RDBMS (abstract base class).
     """
 
     # Start attribute config meta
@@ -42,6 +38,84 @@ class PostgresDbInput(Input):
         """
         pass
 
+    @Config(ptype=str, required=False, default=None)
+    def table(self):
+        """
+        Table name
+        """
+        pass
+
+    @Config(ptype=str, required=False, default=None)
+    def column_names(self):
+        """
+        Column names to populate records with. If empty taken from table metadata.
+        """
+        pass
+
+    @Config(ptype=bool, required=False, default=False)
+    def read_once(self):
+        """
+        Read once? i.e. only do query once and stop
+        """
+        pass
+
+    @Config(ptype=str, required=False, default=None)
+    def query(self):
+        """
+        The query (string) to fire.
+        """
+        pass
+    # End attribute config meta
+
+    def __init__(self, configdict, section, produces):
+        DbInput.__init__(self, configdict, section, produces=produces)
+        self.columns = self.column_names
+        self.select_all = "select * from %s" % self.table
+
+    def result_to_record_array(self, db_records):
+
+        # record is Python list of Python dict (multiple records)
+        records = list()
+
+        # Convert list of lists to list of dict using column_names
+        for db_record in db_records:
+            records.append(dict(zip(self.columns, db_record)))
+
+        return records
+
+    def do_query(self, query_str):
+        """
+        Performs DB-specific  query and returns records iter.
+        """
+        pass
+
+    def read(self, packet):
+
+        # Perform DB-specific query
+        db_records = self.do_query(self.query)
+
+        # Convert query result to record_array
+        packet.data = self.result_to_record_array(db_records)
+
+        # No more records to process?
+        if len(packet.data) == 0 or self.read_once is True:
+            packet.set_end_of_stream()
+            log.info('Nothing to do. All file_records done')
+            return packet
+
+        return packet
+
+
+class PostgresDbInput(SqlDbInput):
+    """
+    Input by querying records from a Postgres database.
+    Input is a query, like SELECT * from mytable.
+    Output is zero or more records as record array (array of dict) or single record (dict).
+
+    produces=FORMAT.record_array (default) or FORMAT.record
+    """
+
+    # Start attribute config meta
     @Config(ptype=str, required=False, default='localhost')
     def host(self):
         """
@@ -69,39 +143,10 @@ class PostgresDbInput(Input):
         Schema (postgres schema) name
         """
         pass
-
-    @Config(ptype=str, required=False, default=None)
-    def table(self):
-        """
-        Table name
-        """
-        pass
-
-    @Config(ptype=str, required=False, default=None)
-    def column_names(self):
-        """
-        Column names to populate records with
-        """
-        pass
-
-    @Config(ptype=bool, required=False, default=False)
-    def read_once(self):
-        """
-        Read once? i.e. only do query once and stop
-        """
-        pass
-
-
-    @Config(ptype=str, required=False, default=None)
-    def query(self):
-        """
-        The query (string) to fire.
-        """
-        pass
     # End attribute config meta
 
     def __init__(self, configdict, section):
-        Input.__init__(self, configdict, section, produces=[FORMAT.record_array, FORMAT.record])
+        SqlDbInput.__init__(self, configdict, section, produces=[FORMAT.record_array, FORMAT.record])
         self.db = None
 
     def init(self):
@@ -128,22 +173,47 @@ class PostgresDbInput(Input):
         db_records = self.db.cursor.fetchall()
         log.info('read recs: %d' % len(db_records))
 
-        # record is Python list of Python dict (multiple records)
-        records = list()
+        return db_records
 
-        # Convert list of lists to list of dict using column_names
-        for db_record in db_records:
-            records.append(dict(zip(self.columns, db_record)))
+class SqliteDbInput(SqlDbInput):
+    """
+    Input by querying records from a SQLite database.
+    Input is a query, like SELECT * from mytable.
+    Output is zero or more records as record array (array of dict) or single record (dict).
 
-        return records
+    produces=FORMAT.record_array (default) or FORMAT.record
+    """
 
-    def read(self, packet):
-        packet.data = self.do_query(self.query)
 
-        # No more records to process?
-        if len(packet.data) == 0 or self.read_once is True:
-            packet.set_end_of_stream()
-            log.info('Nothing to do. All file_records done')
-            return packet
+    def __init__(self, configdict, section):
+        SqlDbInput.__init__(self, configdict, section, produces=[FORMAT.record_array, FORMAT.record])
+        self.db = None
 
-        return packet
+        import sqlite3
+        self.sqlite = sqlite3
+
+    def get_conn(self):
+        # Database name is the filepath
+        log.info('Connect to SQLite DB: %s' % self.database_name)
+        return self.sqlite.connect(self.database_name)
+
+    def init(self):
+        # If no explicit column names given, get from DB meta info
+        if self.column_names is None:
+            # Connect only once to DB
+            conn = self.get_conn()
+            cursor = conn.cursor()
+            cursor.execute(self.select_all)
+            self.columns = [f[0] for f in cursor.description]
+            conn.close()
+
+    def do_query(self, query_str):
+        # We open and close immediately. TODO: maybe this is not necessary i.e. once in init/exit.
+        conn = self.get_conn()
+        cursor = conn.cursor()
+        cursor.execute(query_str)
+        db_records = cursor.fetchall()
+        log.info('%d records read' % len(db_records))
+        conn.close()
+
+        return db_records
